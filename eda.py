5767e2e3753354b977ce3e515c5560a4f299a794
KyungHyun Jovinus Lee <lkh256@gmail.com>
2021-06-10 14:18:53 +0900

    first commit

---
+++ eda.py
@@ -0,0 +1,9 @@
+# %%
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from IPython.display import display
+# %%
+df_orig = pd.read_csv('./data/train.csv')
+df_orig.head()
+# %%
